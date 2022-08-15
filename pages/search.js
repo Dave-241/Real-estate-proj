2174f8c7560c99022e5f6a6178bcd5ad585f684d
DAVIS ISIBOR <davisisibor@gmail.com>
2022-08-15 10:04:13 +0100

    upload

--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import Image from 'next/image'
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
-import Property from '../components/Property';
+import Property from '../components/property';
 
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 import noresult from '../assets/noresult.svg'
